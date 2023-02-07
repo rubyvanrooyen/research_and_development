67461d31941112bfa048dc7f2496dc26b28302ba
Ruby Van Rooyen <ruby@bruce.science.kat.ac.za>
2023-02-07 12:23:37 +0200

    stupid bash script to get all the data currently unstaged in archive

---
+++ cometdata/runme.sh
@@ -0,0 +1,100 @@
+#! /bin/bash
+
+#-----
+# 294G    1629245556_sdp_l0.ms (has target)
+echo
+dataset="1629245556_sdp_l0"
+echo "Getting data set: $dataset"
+
+# echo mvftoms.py -a -f --flags cam <katdaltoken>
+katdaltoken="https://archive-gw-1.kat.ac.za/1629245556/1629245556_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNjc1NzUxMzA5LCJwcmVmaXgiOlsiMTYyOTI0NTU1NiJdLCJleHAiOjE2NzYzNTYxMDksInN1YiI6InJ1YnlAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.DMstQcE6iPK9G769dN75NAkHbpe8EGdzjyfhZ5coqqXhZGw7Wzm4AC0R9bgnK4kq5EXBjKdewHATfmlsm1bMQw"
+mvftoms.py -a -f --flags cam $katdaltoken
+python get_listobs.py $dataset".ms"
+#-----
+
+#-----
+# 350G    1630886477_sdp_l0.ms (has target)
+echo
+dataset="1630886477_sdp_l0"
+echo "Getting data set: $dataset"
+
+katdaltoken="https://archive-gw-1.kat.ac.za/1630886477/1630886477_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNjc1NzUxMzA5LCJwcmVmaXgiOlsiMTYzMDg4NjQ3NyJdLCJleHAiOjE2NzYzNTYxMDksInN1YiI6InJ1YnlAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.y5ZlIjWynHal9Mri2QXZp1FXP7yR5P_H4nJH0kcpnKOJA4CQHQ4Nz6v-AWAsI76gR1WxuCWVL7XvTk3gK19KyQ"
+mvftoms.py -a -f --flags cam $katdaltoken
+python get_listobs.py $dataset".ms"
+#-----
+
+
+#-----
+# 319G    1632889052_sdp_l0.ms (has target)
+echo
+dataset="1632889052_sdp_l0"
+echo "Getting data set: $dataset"
+
+katdaltoken="https://archive-gw-1.kat.ac.za/1632889052/1632889052_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNjc1NzUxMzA5LCJwcmVmaXgiOlsiMTYzMjg4OTA1MiJdLCJleHAiOjE2NzYzNTYxMDksInN1YiI6InJ1YnlAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.P_XGoFLjJvpslh1zPR-s7HJWam4rAUbl6HuMqn-MCHohB8c4fhuJt4mhU1mUg2JMJmf4UlTtd30CRgBJLfjyxQ"
+mvftoms.py -a -f --flags cam $katdaltoken
+python get_listobs.py $dataset".ms"
+#-----
+
+
+#-----
+# 380G    1634435139_sdp_l0.ms (has target)
+echo
+dataset="1634435139_sdp_l0"
+echo "Getting data set: $dataset"
+
+katdaltoken="https://archive-gw-1.kat.ac.za/1634435139/1634435139_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNjc1NzUxMzA5LCJwcmVmaXgiOlsiMTYzNDQzNTEzOSJdLCJleHAiOjE2NzYzNTYxMDksInN1YiI6InJ1YnlAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.52fLPCYFfJuQ4lXD0VFDiRM8-JFMry5ANMmsrliLO2vDSU7yv8Kx3IZQkrhFWd-orCUvSYdswcX6uJJcTjcZaA"
+mvftoms.py -a -f --flags cam $katdaltoken
+python get_listobs.py $dataset".ms"
+#-----
+
+
+#-----
+# 402G    1636435270_sdp_l0.ms (tiny not sure)
+echo
+dataset="1636435270_sdp_l0"
+echo "Getting data set: $dataset"
+
+katdaltoken="https://archive-gw-1.kat.ac.za/1636435270/1636435270_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNjc1NzUxMzA5LCJwcmVmaXgiOlsiMTYzNjQzNTI3MCJdLCJleHAiOjE2NzYzNTYxMDksInN1YiI6InJ1YnlAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.oiDs0AhoOvTV5vyiTWz9q6BfexTuzZB4x8wIt1veKixTk2Pd5L7juC-BI5hfm-_mxuhUFyddztEWGI1cKwkLhQ"
+mvftoms.py -a -f --flags cam $katdaltoken
+python get_listobs.py $dataset".ms"
+#-----
+
+
+#-----
+# 356G    1636601859_sdp_l0.ms (has target)
+echo
+dataset="1636601859_sdp_l0"
+echo "Getting data set: $dataset"
+
+katdaltoken="https://archive-gw-1.kat.ac.za/1636601859/1636601859_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNjc1NzUxMzA4LCJwcmVmaXgiOlsiMTYzNjYwMTg1OSJdLCJleHAiOjE2NzYzNTYxMDgsInN1YiI6InJ1YnlAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.2trnqwV-uBEwkAvy9Ch2ocf5MiXQcDXhLh_Mo08P2VBvncPI-Wp1Gdn_jwrDyX4oR39nEdkyvfztxKoAa4dIOA"
+mvftoms.py -a -f --flags cam $katdaltoken
+python get_listobs.py $dataset".ms"
+#-----
+
+
+#-----
+# 373G    1640311283_sdp_l0.ms (has target)
+echo
+dataset="1640311283_sdp_l0"
+echo "Getting data set: $dataset"
+
+katdaltoken="https://archive-gw-1.kat.ac.za/1640311283/1640311283_sdp_l0.full.rdb?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJrYXQtYXJjaGl2ZS5rYXQuYWMuemEiLCJhdWQiOiJhcmNoaXZlLWd3LTEua2F0LmFjLnphIiwiaWF0IjoxNjc1NzUxMzA4LCJwcmVmaXgiOlsiMTY0MDMxMTI4MyJdLCJleHAiOjE2NzYzNTYxMDgsInN1YiI6InJ1YnlAc2FyYW8uYWMuemEiLCJzY29wZXMiOlsicmVhZCJdfQ.cwTceHWuURVf1lLhJmoR01g3cGuZiHsRs5CnqNb97tH636516c0Sofr3mM4vLK-Vn2pbSofYgbjyIqxfl32Zpw"
+mvftoms.py -a -f --flags cam $katdaltoken
+python get_listobs.py $dataset".ms"
+#-----
+
+
+# #-----
+# echo
+# dataset=""
+# echo "Getting data set: $dataset"
+# 
+# katdaltoken=""
+# echo mvftoms.py -a -f --flags cam $katdaltoken
+# 
+# echo python get_listobs.py $dataset".ms"
+# #-----
+
+du -hcs *.ms
+
+# -fin-
